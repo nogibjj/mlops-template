af35d362ca22b8aaaa72c8492c05efd82153714e
Tigran Harutyunyan <harutyunyan.tigran.v@gmail.com>
2022-11-06 23:47:29 -0500

    Revert "adding tensorflow requirements in Dockerfile"

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 source /home/codespace/venv/bin/activate
 #append it to bash so every shell launches with it 
 echo 'source /home/codespace/venv/bin/activate' >> ~/.bashrc
-#make install-tensorflow
+make install-tensorflow
