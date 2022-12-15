62f01ac106285df7694ef20b859b661a9f1b08c9
Noah Gift <noah.gift@gmail.com>
2022-12-15 15:40:13 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,4 @@
 source /home/codespace/venv/bin/activate
 #append it to bash so every shell launches with it 
 echo 'source /home/codespace/venv/bin/activate' >> ~/.bashrc
-#Below only used if conda is needed)
-#make install-tensorflow-conda 
 
